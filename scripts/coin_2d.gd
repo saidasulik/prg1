1518e3ee2d19d44bf109b6caacde9c5b7829b66f
Sulik <saidasulik37@gmail.com>
2026-07-19 20:27:40 +0300

    coins!

---
+++ scripts/coin_2d.gd
@@ -0,0 +1,15 @@
+extends CollisionShape2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print('монета' ) 
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_coin_body_entered(body: Node2D) -> void:
+	print('нет монетки') # Replace with function body.
