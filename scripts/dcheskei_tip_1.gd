144fc0d5b5d51c8288d56dc1f68aa8968c09e136
Sulik <saidasulik37@gmail.com>
2026-08-29 21:55:01 +0300

    add background and update of death

--- scripts/dcheskei_tip_1.gd
+++ scripts/dcheskei_tip_1.gd
@@ -1,6 +1,9 @@
 extends Node2D
 var speed = 10
 var direction = 1
+@onready var rayright: RayCast2D = $Rayright
+@onready var rayleft: RayCast2D = $Rayleft
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,4 +12,11 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if rayright.is_colliding():
+		direction = -1
+		animated_sprite_2d.flip_h = true
+	if rayleft.is_colliding():
+		direction = +1
+		animated_sprite_2d.flip_h = false
 	position.x = position.x + direction * speed * delta
+	
