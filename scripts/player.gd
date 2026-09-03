c89369eefba3709a47dd5077a40b498d61c9c847
Sulik <saidasulik37@gmail.com>
2026-09-03 15:21:47 +0300

    add super fruts

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,7 @@
 extends CharacterBody2D
 
-
 const SPEED = 150.0
 const JUMP_VELOCITY = -340.0
-
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -13,6 +11,9 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
+	if Input.is_key_pressed(KEY_RIGHT):
+		pass
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -21,5 +22,5 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		
 	move_and_slide()
